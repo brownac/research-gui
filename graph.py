8d065a8b29d0cace53fc70d197b8f45dfb0c28c2
brownac <austincbrown@ufl.edu>
2017-01-25 11:31:37 -0500

    changed pretty much everything

--- graph.py
+++ graph.py
@@ -1,50 +1,87 @@
-from Tkinter import *
+import matplotlib
 
-class Application(Frame):
-    # write to a list
-    def write_to_list(self, *args):
-        self.vec.extend(args)
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 
-    # inhibitory response
-    def is_inhib(self):
-        self.write_to_list(1)
-        print self.vec
+import Tkinter as tk
+import ttk
 
-    # not an inhibitory response
-    def no_inhib(self):
-        self.write_to_list(0)
-        print self.vec
+LARGE_FONT = ("Verdana", 12)
 
 
-    def createWidgets(self):
-        # canvas
-        self.canvas = Canvas(self)
-        self.canvas["bd"] = 3
-        self.canvas["height"] = 500
-        self.canvas["width"] = 500
-        self.canvas.pack()
+class research_gui(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        # yes button
-        self.inhib = Button(self)
-        self.inhib["text"] = "Yes"
-        self.inhib["fg"]   = "green"
-        self.inhib["command"] =  self.is_inhib
-        self.inhib.pack({"side": "left"})
+        tk.Tk.wm_title(self, "Research Gui")
 
-        # no button
-        self.not_inhib = Button(self)
-        self.not_inhib["text"] = "No",
-        self.not_inhib["command"] = self.no_inhib
-        self.not_inhib.pack({"side": "right"})
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    def __init__(self, master=None):
+        self.frames = {}
+
         self.vec = []
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
 
-root = Tk()
-root.title("Research GUI")
-app = Application(master=root)
+        for F in (StartPage, GraphPage):
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+    def fill_vec(self, *args):
+        self.vec.extend(args)
+        print(self.vec)
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button = ttk.Button(self, text="Graph Page", command=lambda: controller.show_frame(GraphPage))
+        button.pack()
+
+
+class GraphPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                             command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+        yes_button = tk.Button(self, text="Yes",
+                                command=lambda: controller.fill_vec(1))
+        yes_button.pack({"side":"left"})
+
+        no_button = tk.Button(self, text="No",
+                              command=lambda: controller.fill_vec(0))
+        no_button.pack({"side":"right"})
+
+
+app = research_gui()
 app.mainloop()
-root.destroy()
